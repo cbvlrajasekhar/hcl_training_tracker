d0947c7632643544710273ef7b503ea72c7fceeb
saipriya <saipriya4242@gmail.com>
2022-05-17 11:21:06 +0530

    trainingmanagement api is created and committed

---
+++ trainingmanagementapi/src/main/java/in/hcl/trainingmanagementapi/TrainingmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package in.hcl.trainingmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingmanagementapiApplication.class, args);
+	}
+
+}
