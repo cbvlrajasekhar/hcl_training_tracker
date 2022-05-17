c621ac93d61fd63361b16acd624d2023515535dd
Prajwal A <prajwal_a@hcl.com>
2022-05-17 10:46:51 +0530

    schedule api is added and commited

---
+++ scheduleapi/src/main/java/in/hcl/scheduleapi/ScheduleapiApplication.java
@@ -0,0 +1,13 @@
+package in.hcl.scheduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleapiApplication.class, args);
+	}
+
+}
